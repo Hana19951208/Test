d94b4ed156cccbfd8686cbe464fe3d735e3441d7
wufangen <1948978861@qq.com>
2020-05-19 11:05:40 +0800

    master 解决冲突

--- 123.sql
+++ 123.sql
@@ -4,6 +4,8 @@ INSERT INTO `` VALUES ('5c052f14e16c2122bc4e8327', 'supplier', '$2a$10$LTaGt1qQq
 INSERT INTO `` VALUES ('5c0789dab8cce13024322b16', '1297564418@qq.com', '$2a$10$DPgcqId8zTtJwRZZag6fu.BysE3nvMrynY9JeHLIuD5PKTEb1sHpy', NULL, '1297564418@qq.com', NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, '1', '0', '2018-12-5 16:19:52', NULL, '2018-12-5 16:19:52', NULL, 'A', NULL, '0', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'thdt');
 INSERT INTO `` VALUES ('system', 'system', '$2a$10$coRU2p4GRrdKKvlfCAzVd.va1N4OJgYQTrTJh9xJyjRINnnJXaeHi', '超级管理员', 'yangdong@ithinkdt.com', '0', '', NULL, '', '2018-9-25 02:03:08', '192.168.16.114', 21140, '2018-6-28 20:16:40', '1', '0', '2015-11-24 12:12:52', 'admin', '2019-3-10 11:18:58', NULL, 'A', '', '0', 'primary', '', '', '', '', '', NULL, '91fd451a-d67a-48fa-8a36-f9a9df78e002', '?????', NULL, '', 'thdt');
 
+
+----first
 -- 这是dev开发的代码 v1.0.0 2020-5-19 11:02:31
 
 -- 这是dev开发的代码 v1.0.1 2020-5-19 11:03:28
